13e1730d524216ca699c7d9eb46052df4e017be7
DhavalSuthar-24 <dhavalll63@gmail.com>
2024-03-27 19:13:27 +0530

    STRIPEEEEEEEEEEEEEEEEE

--- client/src/Components/ProductView.jsx
+++ client/src/Components/ProductView.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { loadStripe } from '@stripe/stripe-js';
 
 const ProductView = () => {
   const { id } = useParams(); // Get the product ID from the URL params
@@ -7,6 +8,9 @@ const ProductView = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [couponCode, setCouponCode] = useState("");
   const [discountApplied, setDiscountApplied] = useState(false);
+  const [discountType, setDiscountType] = useState("");
+  const [discountValue, setDiscountValue] = useState(0);
+  const [discountedPrice, setDiscountedPrice] = useState(0);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -40,33 +44,119 @@ const ProductView = () => {
         throw new Error('Failed to check coupon validity');
       }
       const data = await response.json();
-      if (data.valid) {
+      if (!data.coupon.isUsed && !discountApplied) { // Check if coupon is not used and discount is not already applied
         setDiscountApplied(true);
         // Extract discount information from the coupon object
         const { discountType, discountValue } = data.coupon;
-        // Update the product price based on the validity of the coupon
+        setDiscountType(discountType);
+        setDiscountValue(discountValue);
+        // Calculate discounted price
+        let newDiscountedPrice;
         if (discountType === 'percentage') {
           // Apply percentage discount
-          setProduct(prevProduct => ({
-            ...prevProduct,
-            price: prevProduct.price * (1 - discountValue / 100) // Apply percentage discount
-          }));
+          newDiscountedPrice = (product.price * (1 - discountValue / 100)).toFixed(2);
         } else {
           // Apply fixed value discount
-          setProduct(prevProduct => ({
-            ...prevProduct,
-            price: prevProduct.price - discountValue // Apply fixed value discount
-          }));
+          newDiscountedPrice = (product.price - discountValue).toFixed(2);
         }
-      } else {
-        setDiscountApplied(false);
+        setDiscountedPrice(newDiscountedPrice);
+        // Update the product price based on the validity of the coupon
+        setProduct(prevProduct => ({
+          ...prevProduct,
+          price: newDiscountedPrice // Set the discounted price
+        }));
+        console.log('Updated price:', newDiscountedPrice); // Log the updated price
       }
     } catch (error) {
       console.error(error);
       // Handle error
     }
   };
+
+
+  const makePayment = async () => {
+    const stripe = await loadStripe('pk_test_51OySR3SH9ySesKUUIvonoDQvGUBaP67vQjXrPdfTulRuObuqvoUk0TCooKCI7jOyFOGRmWQXL4poJucjbqk8k33v00WVPocAY2');
   
+    try {
+      // Update product price to discountedPrice
+      const updatedProduct = { ...product, price: discountedPrice,quantity:1 };
+  
+      const body = {
+        product: updatedProduct
+      };
+  console.log(body)
+      const headers = {
+        'Content-Type': 'application/json'
+      };
+  
+      const response = await fetch('api/stripe/create-checkout-session', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body)
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to create checkout session');
+      }
+  
+      const session = await response.json();
+  
+      const result = await stripe.redirectToCheckout({
+        sessionId: session.id
+      });
+  
+      if (result.error) {
+        throw new Error(result.error.message);
+      }
+    } catch (error) {
+      console.error("Error creating checkout session:", error);
+      // Handle error, display message to user, etc.
+    }
+  };
+  // const makePayment = async () => {
+  //   const stripe = await loadStripe('pk_test_51OySR3SH9ySesKUUIvonoDQvGUBaP67vQjXrPdfTulRuObuqvoUk0TCooKCI7jOyFOGRmWQXL4poJucjbqk8k33v00WVPocAY2');
+
+  //   try {
+  //     const updatedProduct = { ...product, price: discountedPrice };
+  //     const body = {
+  //       product: updatedProduct
+  //     };
+
+  //     const headers = {
+  //       'Content-Type': 'application/json'
+  //     };
+
+  //     const response = await fetch('api/stripe/create-checkout-session', {
+  //       method: 'POST',
+  //       headers: headers,
+  //       body: JSON.stringify(body)
+  //     });
+
+  //     if (!response.ok) {
+  //       throw new Error('Failed to create checkout session');
+  //     }
+
+  //     const session = await response.json();
+
+  //     const result = await stripe.redirectToCheckout({
+  //       sessionId: session.id
+  //     });
+
+  //     if (result.error) {
+  //       throw new Error(result.error.message);
+  //     }
+  //   } catch (error) {
+  //     console.error("Error creating checkout session:", error);
+  //     // Handle error, display message to user, etc.
+  //   }
+  // };
+
+  useEffect(() => {
+    if (product) {
+      console.log('Product:', product);
+    }
+  }, [product]);
+
   if (!product) {
     return <div>Loading...</div>;
   }
@@ -105,10 +195,29 @@ const ProductView = () => {
           {product.category}
         </span>
         <h1 className="text-3xl font-bold mb-4">{product.title}</h1>
-        <p className="text-lg text-gray-800 mb-4">${product.price}</p>
+        <div className="mb-4">
+          <p className="text-lg text-gray-800">
+            {discountApplied ? (
+              <span>
+                <span className="text-red-500 line-through">${product.price}</span>
+                &nbsp; &nbsp;
+                {discountType === 'percentage' ? (
+                  <span>{discountValue}% Discount Applied</span>
+                ) : (
+                  <span>${discountValue} Discount Applied</span>
+                )}
+                &nbsp; &nbsp;
+                <br />
+                <span className="font-bold">New Price: ${discountedPrice}</span>
+              </span>
+            ) : (
+              <span>${product.price}</span>
+            )}
+          </p>
+        </div>
         <div className="mb-4">
           <button className="bg-blue-500 text-white px-4 py-2 mr-2 rounded hover:bg-blue-600 transition duration-300">Add to Cart</button>
-          <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300">Buy Now</button>
+          <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300" onClick={makePayment}>Buy Now</button>
         </div>
         <p className="text-gray-700">{product.content}</p>
         <div className="mt-4">
