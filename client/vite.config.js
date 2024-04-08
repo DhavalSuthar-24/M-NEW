57e14d9a184da8eadcafa5b02a1d4d9442435734
DhavalSuthar-24 <dhavalll63@gmail.com>
2024-04-08 15:55:38 +0530

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  rollupOptions: {
+    external: ['xlsx']
+  }
+,
   server:{
     proxy:{
       '/api': {
